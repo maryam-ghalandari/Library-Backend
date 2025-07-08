43bd8e471a64453f9ad9a641ad16033f872f43b2
maryam-ghalandari <mary.shamami@gmail.com>
2025-07-09 02:59:52 +0330

    connect to front

--- Program.cs
+++ Program.cs
@@ -1,100 +1,114 @@
 using System.Reflection.Metadata.Ecma335;
 using System.Runtime.InteropServices;
+using Microsoft.AspNetCore.Mvc;
 using novin_library_backend.DbContextes;
 using novin_library_backend.Entities.Base;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddOpenApi();
+builder.Services.AddCors();
+builder.Services.AddDbContext<LibraryDB>();
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
-
+app.UseCors(policy =>
+{
+    policy
+    .AllowAnyHeader()
+    .AllowAnyMethod()
+    .AllowAnyOrigin();
+});
 app.UseHttpsRedirection();
 
-app.MapGet("books/list", () =>
+app.MapGet("books/list", ([FromServices] LibraryDB db) =>
 {
-    using var db = new LibraryDB();
     return db.Books.ToList();
 });
-app.MapPost("books/creat", (Book book) =>
+app.MapPost("books/creat", (
+    [FromServices] LibraryDB db, [FromBody] Book book) =>
 {
-    using var db = new LibraryDB();
     db.Books.Add(book);
     db.SaveChanges();
-    return "Book created!";
+    return new { IsOk = true, Result = "Book created!" };
 });
-app.MapPut("books/update/{id}", (int id, Book book) =>
+app.MapPut("books/update/{id}", (
+    [FromServices] LibraryDB db,
+    [FromRoute] int id,
+    [FromBody] Book book) =>
 {
-    using var db = new LibraryDB();
     var b = db.Books.Find(id);
     if (b == null)
     {
-        return "لطفا مقدار صحیح را وارد کنیذ";
+        return new { IsOk = false, Result = "Book Not Found" };
     }
     b.Title = book.Title;
     b.Writer = book.Writer;
     b.Publisher = book.Publisher;
     b.Price = book.Price;
     db.SaveChanges();
-    return "Book updated!";
+    return new { IsOk = true, Result = "Book updated!" };
 });
-app.MapDelete("books/remove/{id}", (int id) =>
+app.MapDelete("books/remove/{id}", (
+    [FromServices] LibraryDB db,
+    [FromRoute] int id) =>
 {
-    using var db = new LibraryDB();
-    var b = db.Books.Find(id);
-    if (b == null)
+    var book = db.Books.Find(id);
+    if (book == null)
     {
-        return "Book Not Found";
+        return new { IsOk = false, Result = "Book Not Found" };
     }
-    db.Books.Remove(b);
+    db.Books.Remove(book);
     db.SaveChanges();
-    return "Book removed!";
+    return new { IsOk = true, Result = "Book removed!" };
 });
 
 
-app.MapGet("members/list", () =>
+app.MapGet("members/list", ([FromServices] LibraryDB db) =>
 {
-    using var db = new LibraryDB();
     return db.Members.ToList();
 
 });
 
-app.MapPost("members/Creat", (Member member) =>
+app.MapPost("members/Creat", (
+    [FromServices] LibraryDB db,
+    [FromBody] Member member) =>
 {
-    using var db = new LibraryDB();
     db.Members.Add(member);
     db.SaveChanges();
-    return "New Member Created";
+    return new { IsOk = true, Result = "member created!" };
 });
-app.MapPut("members/update/{id}", (int id, Member member) =>
+app.MapPut("members/update/{id}", (
+    [FromServices] LibraryDB db,
+    [FromRoute] int id,
+    [FromBody] Member member) =>
 {
-    using var db = new LibraryDB();
     var m = db.Members.Find(id);
     if (m == null)
     {
-        return "Member Not Found";
+        return new { IsOk = false, Result = "Member Not Found" };
     }
     m.Firstname = member.Firstname;
     m.Lastname = member.Lastname;
     m.Gender = member.Gender;
     db.SaveChanges();
-    return "مشخصات به روز رسانی شد";
+    return new { IsOk = true, Result = "Member Updated!" };
 });
 
-app.MapDelete("members/remove/{id}", (int id) =>
+app.MapDelete("members/remove/{id}", (
+    [FromServices] LibraryDB db,
+    [FromRoute] int id) =>
 {
-    using var db = new LibraryDB();
     var m = db.Members.Find(id);
     if (m == null)
     {
-        return "Member Not Found!";
+        return new { IsOk = false, Result = "Member Not Found!" };
     }
     db.Members.Remove(m);
     db.SaveChanges();
-    return "Member Removed!";
+    return new { IsOk = true, Result = "Member Removed!" };
 });
 
 app.Run();
