5b011259bc9c33ae1e62ec96d579ff6b70a61653
maryam-ghalandari <mary.shamami@gmail.com>
2025-07-10 19:27:29 +0330

    migration W04041902

---
+++ Migrations/20250710154742_W04041902.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace novin_library_backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class W04041902 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
